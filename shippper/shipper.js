5af7a2a8e39a9259ef0f9d9cb0e8546c39a56aca
Sorangez <119031998+Sorangez@users.noreply.github.com>
2023-09-24 02:22:27 +0700

    Add files via upload

---
+++ shippper/shipper.js
@@ -0,0 +1,8 @@
+const saveButton = document.getElementById('saveButton');
+const roleDropdown = document.getElementById('status-dropdown');
+
+    saveButton.addEventListener('click', function () {
+    const selectedStatus = roleDropdown.value;
+    alert(`Selected status: ${selectedStatus}`);
+});
+
